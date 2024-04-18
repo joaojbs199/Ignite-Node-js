8d45c9849131bb1c29a8333a60ba1ec91066a64d
joaojbs199 <joaojbs199@gmail.com>
2024-04-18 06:19:20 -0300

    feat: create register use case

---
+++ Implementando o SOLID/src/use-cases/register.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+
+interface RegisterUseCaseParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const registerUseCase = async ({ name, email, password }: RegisterUseCaseParams) => {
+  // eslint-disable-next-line camelcase
+  const password_hash = await hash(password, 10);
+
+  const hasUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (hasUser) {
+    throw new Error('Email already exists.');
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      // eslint-disable-next-line camelcase
+      password_hash,
+    },
+  });
+};
