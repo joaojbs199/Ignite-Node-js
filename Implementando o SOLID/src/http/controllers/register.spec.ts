6a9131368634bc852ebbca967610a447ee192097
joaojbs199 <joaojbs199@gmail.com>
2024-04-26 10:19:35 -0300

    test: add e2e test to register user

--- Implementando o SOLID/src/http/controllers/register.spec.ts
+++ Implementando o SOLID/src/http/controllers/register.spec.ts
@@ -1,5 +1,22 @@
-import { expect, test } from 'vitest';
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '@/app';
 
-test('ok', () => {
-  expect(1 + 1).toBe(2);
+describe('Register e2e', () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john_doe@test.com',
+      password: 'testpass',
+    });
+
+    expect(response.status).toEqual(201);
+  });
 });
