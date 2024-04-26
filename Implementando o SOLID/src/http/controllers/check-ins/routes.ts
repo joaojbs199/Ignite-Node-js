bdd6e7a639ab7a539270c233b2f91d5a7ab41fae
joaojbs199 <joaojbs199@gmail.com>
2024-04-26 17:58:08 -0300

    feat: create RBAC

--- Implementando o SOLID/src/http/controllers/check-ins/routes.ts
+++ Implementando o SOLID/src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { create } from '@/http/controllers/check-ins/create';
 import { validate } from '@/http/controllers/check-ins/validate';
 import { history } from '@/http/controllers/check-ins/history';
 import { metrics } from '@/http/controllers/check-ins/metrics';
+import { verifyUserRole } from '@/http/middlewares/verify-user-role';
 
 export const checkInsRoutes = async (app: FastifyInstance) => {
   app.addHook('onRequest', verifyJwt);
@@ -11,5 +12,5 @@ export const checkInsRoutes = async (app: FastifyInstance) => {
   app.post('/gyms/:gymId/check-ins', create);
   app.get('/check-ins/history', history);
   app.get('/check-ins/metrics', metrics);
-  app.patch('/check-ins/:checkInId/validate', validate);
+  app.patch('/check-ins/:checkInId/validate', { onRequest: [verifyUserRole('ADMIN')] }, validate);
 };
