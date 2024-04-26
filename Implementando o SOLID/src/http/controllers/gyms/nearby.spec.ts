a6b9f3ba7cab2d66b6115702ec81bc866a778d5f
joaojbs199 <joaojbs199@gmail.com>
2024-04-26 18:09:04 -0300

    test: adjust tests

--- Implementando o SOLID/src/http/controllers/gyms/nearby.spec.ts
+++ Implementando o SOLID/src/http/controllers/gyms/nearby.spec.ts
@@ -12,7 +12,7 @@ describe('Nearby Gyms e2e', () => {
     await app.close();
   });
   it('should be able to list nearby gyms', async () => {
-    const { token } = await createAndAuthenticateUser(app);
+    const { token } = await createAndAuthenticateUser(app, true);
 
     await request(app.server).post('/gyms').set('Authorization', `Bearer ${token}`).send({
       title: 'Near gym',
--- Implementando o SOLID/src/utils/test/create-and-authenticate-user.ts
+++ Implementando o SOLID/src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,16 @@
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 import { FastifyInstance } from 'fastify';
 import request from 'supertest';
 
-export const createAndAuthenticateUser = async (app: FastifyInstance) => {
-  await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'john_doe@test.com',
-    password: 'testpass',
+export const createAndAuthenticateUser = async (app: FastifyInstance, isAdmin = false) => {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john_doe@test.com',
+      password_hash: await hash('testpass', 10),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   });
 
   const authResponse = await request(app.server).post('/sessions').send({
