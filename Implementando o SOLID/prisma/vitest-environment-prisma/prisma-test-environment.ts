8584a1a83198e82b0cb60c19cd15d67bcd018c8e
joaojbs199 <joaojbs199@gmail.com>
2024-04-26 10:09:20 -0300

    feat: add flow to create specifica database for each e2e test

--- Implementando o SOLID/prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ Implementando o SOLID/prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,39 @@
+import { randomUUID } from 'crypto';
 import { Environment } from 'vitest';
+import 'dotenv/config';
+import { execSync } from 'child_process';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const generateDatabaseUrl = (schema: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Provide a DATABASE_URL environment variable.');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+};
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup');
+    const schema = randomUUID();
+    const databaseUrl = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync('npx prisma migrate deploy');
 
     return {
-      teardown() {
-        console.log('Teardown');
+      async teardown() {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+
+        await prisma.$disconnect();
       },
     };
   },
